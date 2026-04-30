af85f39d931f631e333397d2fbdbb4d5535cbd1f
AlinaTanchuk <alinka.tanchuk@gmail.com>
2026-04-30 17:25:34 +0300

    Creating interface (Menu)
    Созданы рабочий набросок общего интерфейса (меню) и заготовки некоторых форм, необходимые для работы меню программы.

---
+++ StatAnalyzer/StatAnalyzer/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace StatAnalyzer
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.buttonsPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+
+            // headerPanel
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(34, 87, 169);
+            this.headerPanel.Controls.Add(this.titleLabel);
+            this.headerPanel.Controls.Add(this.subtitleLabel);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Padding = new System.Windows.Forms.Padding(30, 15, 30, 15);
+            this.headerPanel.Size = new System.Drawing.Size(784, 110);
+            this.headerPanel.TabIndex = 0;
+
+            // titleLabel
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(30, 18);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(224, 41);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "📊 Статистический анализ данных России";
+
+            // subtitleLabel
+            this.subtitleLabel.AutoSize = true;
+            this.subtitleLabel.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.subtitleLabel.ForeColor = System.Drawing.Color.FromArgb(180, 210, 255);
+            this.subtitleLabel.Location = new System.Drawing.Point(32, 62);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(262, 19);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "Свежая ежегодная статистика";
+
+            // buttonsPanel
+            this.buttonsPanel.ColumnCount = 2;
+            this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttonsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonsPanel.Location = new System.Drawing.Point(0, 110);
+            this.buttonsPanel.Name = "buttonsPanel";
+            this.buttonsPanel.Padding = new System.Windows.Forms.Padding(40, 30, 40, 30);
+            this.buttonsPanel.RowCount = 2;
+            this.buttonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.buttonsPanel.Size = new System.Drawing.Size(784, 451);
+            this.buttonsPanel.TabIndex = 1;
+
+            // MainForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(245, 247, 250);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.buttonsPanel);
+            this.Controls.Add(this.headerPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Статистический анализатор";
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.TableLayoutPanel buttonsPanel;
+    }
+}
---
+++ StatAnalyzer/StatAnalyzer/Variant9Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatAnalyzer
+{
+    public partial class Variant9Form : Form
+    {
+        public Variant9Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
